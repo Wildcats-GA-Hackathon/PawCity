9abf6b79e1c36cbc4d062ec6327bc80cda2e97a4
Jonathan Lee <jonathan.1994.lee@gmail.com>
2021-09-22 15:23:13 -0700

    Change port to 3000

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 mongoose
 	.connect(process.env.CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
-	.then(() =>
+	.then((instance) =>
 		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
 	)
 	.catch((error) => console.log(error.message));
